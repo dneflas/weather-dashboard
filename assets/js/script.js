667e9596a3b32d632e1f3e2c3b83706862a39cf2
Debbie Neflas <debbieneflas@gmail.com>
2022-04-12 14:45:56 -0700

    append recent searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,23 @@ var searchContainer = document.getElementById("search-container");
 var resultContainer = document.getElementById("result-container");
 var userFormEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#city-input");
+var recentSearchesEl = document.querySelector("#recent-searches")
+var recentSearches = [];
+
+var LoadRecentSearches = function(){
+    recentStoredSearches = JSON.parse(localStorage.getItem("recentStoredSearches"));
+
+    if(!recentStoredSearches) {
+        recentStoredSearches = recentSearches
+    };
+
+    recentSearches = recentStoredSearches;
+
+    for (var i = 0; i < recentSearches.length; i++){
+        // var city = recentSearches[i].replace("%20", " ")
+        addRecent(recentSearches[i]);
+    }
+}
 
 var formSubmitHandler = function(event){
     event.preventDefault();
@@ -10,9 +27,15 @@ var formSubmitHandler = function(event){
     trim()
     .toLowerCase()
     .replace(" ", "%20");
-    
-    console.log(cityInput);
-    getGeoCode(cityInput);
+
+    if (cityInput){
+        getGeoCode(cityInput);
+        cityInput = "";
+
+
+    } else {
+        alert("Please enter a valid city");
+    }
 
 }
 
@@ -23,16 +46,41 @@ var getGeoCode = function(city){
         .then(function(response){
             if (response.ok){
                 response.json().then(function(data){
+                    console.log(response)
+                    recentSearches.push(city);
+                    localStorage.setItem("recentStoredSearches", JSON.stringify(recentSearches));
+                    addRecent(city);
                    var lat = data[0].lat;
                    var lon = data[0].lon;
-                   console.log(response);
+                   getWeather(lat, lon);
                 });
             } else {
-                alert("Please enter a valid city");
+                alert("Error: City Not Found");
             };
         
-    });
-    
+         })
+         .catch(function(error){
+             alert("Unable to connect to server")
+         });    
 };
 
+var getWeather = function(lat, lon){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=81899826dc7664a048a837db5523dd36";
+    fetch(apiUrl)
+        .then(function(response){
+            console.log(response);
+        });
+};
+
+var addRecent = function(city){
+    var recentSearchItem = document.createElement("button");
+    recentSearchItem.className = "d-block btn btn-secondary"
+    recentSearchItem.textContent = city.replace("%20", " ");
+
+    recentSearchesEl.appendChild(recentSearchItem);
+}
+
+
+
+LoadRecentSearches();
 userFormEl.addEventListener("submit", formSubmitHandler);
