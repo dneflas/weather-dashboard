a98d26f1ecdfee9778076211c5e2d87876f1181a
Debbie Neflas <debbieneflas@gmail.com>
2022-04-13 00:40:36 -0700

    get weather variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,10 +15,9 @@ var LoadRecentSearches = function(){
     recentSearches = recentStoredSearches;
 
     for (var i = 0; i < recentSearches.length; i++){
-        // var city = recentSearches[i].replace("%20", " ")
-        addRecent(recentSearches[i]);
+        addRecent(recentSearches[i].city);
     }
-}
+};
 
 var formSubmitHandler = function(event){
     event.preventDefault();
@@ -26,10 +25,11 @@ var formSubmitHandler = function(event){
     var cityInput = cityInputEl.value.
     trim()
     .toLowerCase()
-    .replace(" ", "%20");
+
 
     if (cityInput){
         getGeoCode(cityInput);
+        addRecent(cityInput);
         cityInput = "";
 
 
@@ -37,7 +37,7 @@ var formSubmitHandler = function(event){
         alert("Please enter a valid city");
     }
 
-}
+};
 
 var getGeoCode = function(city){
     var geoCodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city +"&appid=81899826dc7664a048a837db5523dd36"
@@ -47,11 +47,10 @@ var getGeoCode = function(city){
             if (response.ok){
                 response.json().then(function(data){
                     console.log(response)
-                    recentSearches.push(city);
-                    localStorage.setItem("recentStoredSearches", JSON.stringify(recentSearches));
-                    addRecent(city);
                    var lat = data[0].lat;
                    var lon = data[0].lon;
+                   pushRecent(city, lat, lon);
+
                    getWeather(lat, lon);
                 });
             } else {
@@ -65,22 +64,47 @@ var getGeoCode = function(city){
 };
 
 var getWeather = function(lat, lon){
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=81899826dc7664a048a837db5523dd36";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=81899826dc7664a048a837db5523dd36";
     fetch(apiUrl)
         .then(function(response){
-            console.log(response);
+            response.json().then(function(data){
+                console.log(data);
+                var currentDate = new Date(data.current.dt*1000).toLocaleDateString("en-US");
+                var currentIcon = "http://openweathermap.org/img/wn/" + data.current.weather.icon + "@2x.png"
+                var currentTemp = data.current.temp + "°F";
+                var currentHumidity = data.current.humidity + "%";
+                var currentWindSpeed = data.current.wind_speed + "MPH";
+                var currentUvi = data.current.uvi;
+
+                for (var i = 1; i < 6; i++){
+                    var dailyDate = new Date(data.daily[i].dt*1000).toLocaleDateString("en-US");
+                    var dailyIcon = "http://openweathermap.org/img/wn/" + data.daily[i].weather.icon + "@2x.png"
+                    var dailyTemp = data.daily[i].temp + "°F";
+                    var dailyHumidity = data.daily[i].humidity + "%";
+                    var dailyWindSpeed = data.daily[i].wind_speed + "MPH";
+                    var dailyUvi = data.daily[i].uvi;
+                }
+
+            });
         });
 };
 
 var addRecent = function(city){
     var recentSearchItem = document.createElement("button");
-    recentSearchItem.className = "d-block btn btn-secondary"
-    recentSearchItem.textContent = city.replace("%20", " ");
+    recentSearchItem.className = "recent-search-item d-block btn btn-secondary"
+    recentSearchItem.textContent = city
 
-    recentSearchesEl.appendChild(recentSearchItem);
+    recentSearchesEl.prepend(recentSearchItem);
+};
+
+var pushRecent = function(city, lat, lon){
+    recentSearches.push({"city": city, "lat": lat, "lon": lon});
+    localStorage.setItem("recentStoredSearches", JSON.stringify(recentSearches));
+
+
 }
-
 
 
 LoadRecentSearches();
 userFormEl.addEventListener("submit", formSubmitHandler);
+
